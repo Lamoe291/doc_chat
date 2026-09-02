cc706ea578c16469ed00987559eb113234e0b1a4
Moellenbrok <lmoellenbrock@htc-login03.zib.de>
2026-09-02 21:26:47 +0200

    fix index_store

--- src/doc_chat/index_store.py
+++ src/doc_chat/index_store.py
@@ -19,7 +19,8 @@ class IndexStore:
 
         # Save the chunks associated with the vector store
         with open(chunks_path, "w") as f:
-            json.dump([asdict(chunk) for chunk in vector_store.chunks], f)
+            json.dump([{**asdict(chunk),
+        "source": str(chunk.source)} for chunk in vector_store.chunks], f)
 
 
     def load(self, path: Path) -> VectorStore:
@@ -29,5 +30,8 @@ class IndexStore:
         index = faiss.read_index(str(index_path))
         with open(chunks_path, "r") as f:
             loaded_chunks = json.load(f)
-            chunks = [Chunk(**chunk_dict) for chunk_dict in loaded_chunks]
+            chunks = [Chunk(id=chunk_dict["id"],
+        source=Path(chunk_dict["source"]),
+        page_number=chunk_dict["page_number"],
+        text=chunk_dict["text"]) for chunk_dict in loaded_chunks]
         return VectorStore(embedding_dimension=index.d, index=index, chunks=chunks)
